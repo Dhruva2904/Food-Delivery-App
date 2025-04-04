3d2bbc09c8891d8b4b6e134d6e7b4a471f890d34
Dhruva <dhruvinayak622@gmail.com>
2025-04-04 23:54:54 +0530

    Add files via upload

---
+++ frontend/src/components/NavbarComponent.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavbarComponent({ user }) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        {/* Logo */}
+        <Link className="navbar-brand" to="/">MyApp</Link>
+
+        {/* Mobile Toggle Button */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            {user ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link text-uppercase" to="/profile">
+                    {user.username}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link btn btn-danger text-white ms-2" to="/logout">
+                    Logout
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavbarComponent;
